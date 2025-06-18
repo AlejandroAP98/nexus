c496918b2ff6a636db84f429b7c986ae2361cd33
Alejandro Álvarez <107128294+AlejandroAP98@users.noreply.github.com>
2025-06-18 16:51:33 -0500

    prueba

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="milib-ejemplo",
+    version="0.0.1",
+    author="Alejandro",
+    author_email="aalvarezpa@unal.edu.co",
+    description="Esta es una librería para subir a Nexus",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+)
